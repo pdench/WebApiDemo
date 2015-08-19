1c7a7c602856c55901f412bb0d4d9de1834e150e
pdench <pfdench@gmail.com>
2015-08-19 11:17:07 -0400

    Initial commit: clean solution

---
+++ WebApiDemo_V2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebApiDemo_V2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebApiDemo_V2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiDemo_V2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
